e09549ffe04f07935c7b93edcef428a16ca48bdb
matheus monteiro <matheusyanmonteiro@gmail.com>
2022-05-03 22:33:57 -0300

    create documentation and init user table with clud

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,17 @@ import "./shared/container";
 import "./database";
 import { router } from "./routes";
 import { AppError } from "./errors/AppError";
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(router);
+
 
 
 app.use(
